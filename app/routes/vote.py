7e554e5867c3655761190c7a9c98bf97a81133cf
leonz <medgarxyz@gmail.com>
2025-06-18 14:25:19 -0600

    add schemas

--- app/routes/vote.py
+++ app/routes/vote.py
@@ -1,10 +1,30 @@
-# to test votations
+# add ellection to get voterinos
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from app import schemas, models, database
+from datetime import datetime
 
 router = APIRouter()
 
-@router.get("/ping")
-def ping():
-    return {"message": "pong desde /vote"}
+def get_db():
+    db = database.SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@router.post("/election", response_model=schemas.ElectionOut)
+def create_election(election: schemas.ElectionCreate, db: Session = Depends(get_db)):
+    new_election = models.Election(
+        title=election.title,
+        description=election.description,
+        start_time=election.start_time or datetime.utcnow(),
+        end_time=election.end_time,
+        is_active=True,
+    )
+    db.add(new_election)
+    db.commit()
+    db.refresh(new_election)
+    return new_election
 
